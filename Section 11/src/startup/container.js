1cba082efc020681c478aeb50e9b9fd7da372712
KVeramendi <kevin_veramendi18@hotmail.com>
2025-04-07 08:45:58 -0500

    feat: :sparkles: Agregar repositorios y servicios
    Se agregó los repositorios y servicios de User, Idea y Comment al proyecto

--- Section 11/src/startup/container.js
+++ Section 11/src/startup/container.js
@@ -3,7 +3,7 @@ const { createContainer, asClass, asValue, asFunction } = require('awilix');
 const config = require('../config');
 const app = require('.');
 // services
-const { HomeService } = require('../services');
+const { HomeService, UserService, IdeaService, CommentService } = require('../services');
 // controllers
 const { HomeController } = require('../controllers');
 // routes
@@ -11,6 +11,8 @@ const { HomeRoute } = require('../routes/index.route');
 const Routes = require('../routes');
 // models
 const { User, Idea, Comment } = require('../models');
+// repositories
+const { UserRepository, IdeaRepository, CommentRepository } = require('../repositories');
 
 const container = createContainer();
 container
@@ -20,7 +22,10 @@ container
         config: asValue(config)
     })
     .register({
-        HomeService: asClass(HomeService).singleton()
+        HomeService: asClass(HomeService).singleton(),
+        UserService: asClass(UserService).singleton(),
+        IdeaService: asClass(IdeaService).singleton(),
+        CommentService: asClass(CommentService).singleton()
     })
     .register({
         HomeController: asClass(HomeController.bind(HomeController)).singleton()
@@ -32,6 +37,11 @@ container
         User: asValue(User),
         Idea: asValue(Idea),
         Comment: asValue(Comment)
+    })
+    .register({
+        UserRepository: asClass(UserRepository).singleton(),
+        IdeaRepository: asClass(IdeaRepository).singleton(),
+        CommentRepository: asClass(CommentRepository).singleton()
     });
 
 module.exports = container;
